c7860f932e85a8ba2ec714413ffe22ca914632df
TRUE-EGOISTUS <goldenfreddy6661@gmail.com>
2024-12-26 11:14:28 +0300

    Update photo.js

--- photo.js
+++ photo.js
@@ -18,7 +18,7 @@ document.getElementById("textParagraph_3").addEventListener("click", function() 
     console.log("Фон параграфа изменён"); // Вывод для отладки
 });
 const photo = document.getElementById("Mao");
-const teacherPhotoUrl = "E:/funny-klev-club-fc52-p-smeshnie-kartinki-mao-tszedun-31.jpg";
+const teacherPhotoUrl = "funny-klev-club-fc52-p-smeshnie-kartinki-mao-tszedun-31.jpg";
 photo.onclick = function() {
     photo.src = teacherPhotoUrl;
     photo.style.width = "480px";
